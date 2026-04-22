70a529600ca49a4f561ddbc8c12834c0c6e1fe55
pari1234-c <parinitachoudhari850@gmail.com>
2026-04-22 09:30:57 +0530

    Add addition script to calculate sum of two numbers

---
+++ Basic Programs Shell Script/Addition/addition.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Enter value of a:"
+read a
+
+echo "Enter value of b:"
+read b
+
+sum=$((a + b))
+
+echo "Value of a: $a"
+echo "Value of b: $b"
+echo "Addition of $a and $b is: $sum"
